f5d93ae6e4093a687e3c8cfc08cca5125b2a056b
freetapp <freetapp@freet.co.kr>
2018-11-21 10:39:22 +0900

    test1

--- MainActivity.java
+++ MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String a ="ee1acasdcasdcaasdsd2asd31";
+        String a ="ee1acasdcasdcaasdsd2asd3asd1";
     }
 }
